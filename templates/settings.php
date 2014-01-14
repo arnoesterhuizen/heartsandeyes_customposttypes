7889a9cb3d9237c109481812fb01c45eaf48c172
Arno Esterhuizen <arno.esterhuizen@gmail.com>
2014-01-14 16:47:32 +0200

    Update settings.php

--- templates/settings.php
+++ templates/settings.php
@@ -1,10 +1,10 @@
 <div class="wrap">
-    <h2>WP Plugin Template</h2>
+    <h2>WP Hearts & Eyes Plugin</h2>
     <form method="post" action="options.php"> 
-        <?php @settings_fields('wp_plugin_template-group'); ?>
-        <?php @do_settings_fields('wp_plugin_template-group'); ?>
+        <?php @settings_fields('wp_heartsandeyes_plugin-group'); ?>
+        <?php @do_settings_fields('wp_heartsandeyes_plugin-group'); ?>
 
-        <?php do_settings_sections('wp_plugin_template'); ?>
+        <?php do_settings_sections('wp_heartsandeyes_plugin'); ?>
 
         <?php @submit_button(); ?>
     </form>
